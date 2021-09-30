319fb4f24c72b8c07295261365d7ab4ebcbaf255
Elefly13 <elymara13@gmail.com>
2021-09-30 18:59:12 +0200

    Struttura footer

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,7 +1,29 @@
 <template>
  <div>
+    <div class="wrapper">
 
- </div>
+      <div class="container">
+        <div class="big-box">
+          <div class="box left">
+
+          </div>
+          <div class="box center">
+
+          </div>
+          <div class="box right">
+
+          </div>
+        </div>
+      </div>
+      <div class="container-foot-bottom">
+        <div class="big-box-foot">
+
+        </div>
+
+      </div>
+
+    </div>
+  </div>
 </template>
 
 <script>
@@ -13,5 +35,43 @@ export default {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped lang="scss">
+@import "../style/general.scss";
+@import "../style/vars.scss";
 
+.wrapper {
+  width: 100vw;
+  height: 720px;
+  background-color: $two;
+  .container {
+    display: flex;
+    align-items: center;
+    width: 60%;
+    height: 600px;
+    margin: 0 auto;
+    .big-box {
+      display: flex;
+      width: 100%;
+      height: 400px;
+      background-color: coral;
+      .box {
+        width: calc(100% / 3);
+        height: 100%;
+        border: 1px solid black;
+      }
+    }
+  }
+  .container-foot-bottom {
+    display: flex;
+    align-items: center;
+    width: 100vw;
+    height: 120px;
+    border-top: 1px solid $seven;
+    .big-box-foot {
+      width: 60%;
+      height: 60px;
+      margin: 0 auto;
+      background-color: blanchedalmond;
+    }
+  }
+}
 </style>
--- src/components/Main.vue
+++ src/components/Main.vue
@@ -1,6 +1,11 @@
 <template>
  <div>
+  <div class="wrapper">
+     <div class="container">
 
+     </div>
+
+   </div>
  </div>
 </template>
 
@@ -13,5 +18,13 @@ export default {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped lang="scss">
+@import "../style/general.scss";
+@import "../style/vars.scss";
 
+.wrapper {
+  width: 100vw;
+  height: 720px;
+  background-color: $five;
+  
+}
 </style>
