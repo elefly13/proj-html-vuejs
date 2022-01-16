53882b1c18309aace62bdd1a8c7804a08f665bdb
Elefly13 <elymara13@gmail.com>
2022-01-16 20:15:37 +0100

    pubblica

--- src/main.js
+++ src/main.js
@@ -6,3 +6,9 @@ Vue.config.productionTip = false
 new Vue({
   render: h => h(App),
 }).$mount('#app')
+
+module.exports = {
+  publicPath: process.env.NODE_ENV === 'production'
+    ? '/proj-html-vuejs/'
+    : '/'
+}
